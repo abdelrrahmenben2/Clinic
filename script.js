60971c627ab326505bc6c59187f22e73b9be4c15
bkm-x0 <kder45942@gmail.com>
2025-04-01 23:00:25 +0100

    111

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.oneclick = () =>{
+menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
